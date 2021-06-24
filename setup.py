12e08c9f9240eb8d3670b682012b9363dac44428
Roger Camargo <huogerac@gmail.com>
2021-06-24 16:01:53 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="containerpy",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/huogerac/containerpy",
     license="MIT",
     author="Roger Camargo",
